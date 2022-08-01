40701cdd0b061d0ddcc037cd902671bc3828544f
SergeyKartavcev <102188021+SergeyKartavcev@users.noreply.github.com>
2022-08-01 22:26:58 +0300

    add
    вніс правки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
 const galleryImages = document.querySelector(".gallery");
-const cardMarkup = createGelleryCard (galleryItems);
-galleryImages.insertAdjacentHTML('beforeend', cardMarkup);
-  
-function createGelleryCard (images){
-    return galleryItems.map(({preview, original, description}) =>{
-    return `
+const cardMarkup = createGelleryCard(galleryItems);
+galleryImages.insertAdjacentHTML("beforeend", cardMarkup);
+
+function createGelleryCard(images) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
     <div class="gallery__item">
     <a class="gallery__link" href="${original}">
       <img
@@ -18,42 +19,35 @@ function createGelleryCard (images){
         alt="${description}"
       />
     </a>
-  </div>`; })
-  .join("")
-
+  </div>`;
+    })
+    .join("");
 }
 
+galleryImages.addEventListener("click", createModalImage);
 
-galleryImages.addEventListener('click',createModalImage);  
-
-
-
-function createModalImage (event){
+function createModalImage(event) {
   event.preventDefault();
   const srcImg = event.target.dataset.source;
-const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(
+    `
 class="gallery__image"
 <img  src="${srcImg}">`,
-{  onClose: (instance) => {
-  window.removeEventListener("keydown", onCloseKeyEsc);    
-  }, 
-   }  
-   );
-if (instance.show(instance)) {
-  window.addEventListener("keydown", onCloseKeyEsc); 
- }
-function onCloseKeyEsc(e) {   
-   if (e.code === "Escape" && instance.visible()) { 
-         instance.close(); 
-        } 
-         }
-        };
-
-
-  
-
-  
-
-
-
-
+    {
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onCloseKeyEsc);
+      },
+      onShow: (instance) => {
+        window.addEventListener("keydown", onCloseKeyEsc);
+      },
+    }
+  );
+  if (instance.show(instance)) {
+    window.addEventListener("keydown", onCloseKeyEsc);
+  }
+  function onCloseKeyEsc(e) {
+    if (e.code === "Escape" && instance.visible()) {
+      instance.close();
+    }
+  }
+}
